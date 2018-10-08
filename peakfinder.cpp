207467317818e61326ff27264b656411c51c8ade
joyce214819 <joyce214819@gmail.com>
2018-10-08 14:56:13 +0800

    first commit

---
+++ peakfinder.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+peakfinder //retrun position
+
+
+
+
+
+
+int main(int argv,char* argv[]){
+    int array;
+    cin>>array; //input an array
+
+
+
+    cout<<       //output 
+
+
+
+}
